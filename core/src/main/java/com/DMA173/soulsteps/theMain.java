16d3891973ad4e0aa0fe8d832959ebe3cb907c1a
Deatrax <161139217+Deatrax@users.noreply.github.com>
2025-08-10 05:02:50 +0600

    deleted the entire thing because i forgot to use game template, remade the entire thing using game template

---
+++ core/src/main/java/com/DMA173/soulsteps/theMain.java
@@ -0,0 +1,11 @@
+package com.DMA173.soulsteps;
+
+import com.badlogic.gdx.Game;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class theMain extends Game {
+    @Override
+    public void create() {
+        setScreen(new FirstScreen());
+    }
+}
