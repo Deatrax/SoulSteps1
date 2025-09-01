bd23c700636730601eb92ab6042cb9f52b846ed9
Deatrax <161139217+Deatrax@users.noreply.github.com>
2025-09-01 17:04:21 +0600

    added assets and map with collision

---
+++ assets/pixelinterior_LRK/cabinets_LRK.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="cabinets_LRK" tilewidth="32" tileheight="32" tilecount="7371" columns="117">
+ <image source="cabinets_LRK.png" width="3750" height="2020"/>
+</tileset>
