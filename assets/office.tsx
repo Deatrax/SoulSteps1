bd4b03eadd7f29cefab443a6524bbe22192d1c05
Deatrax <161139217+Deatrax@users.noreply.github.com>
2025-09-03 03:36:33 +0600

    fixed the city map and hidden collision layer on all other maps

--- assets/office.tsx
+++ assets/office.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.2" name="office" tilewidth="32" tileheight="32" tilecount="841" columns="29">
- <image source="office.png" width="950" height="950"/>
+<tileset version="1.10" tiledversion="1.11.2" name="office" tilewidth="32" tileheight="32" tilecount="567" columns="21">
+ <image source="office.png" width="698" height="884"/>
 </tileset>
